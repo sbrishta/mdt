04a2c279d4f38813365ddd5557b9dac32291a8bf
sbrishta <sbrishta.13@gmail.com>
2022-03-07 21:39:59 +0800

    balance and transaction get api integrated

--- app/src/main/java/com/sbr/mdt/api/retrofit/MDTAPI.kt
+++ app/src/main/java/com/sbr/mdt/api/retrofit/MDTAPI.kt
@@ -3,25 +3,27 @@ package com.sbr.mdt.api.retrofit
 import com.sbr.mdt.api.balance.BalanceGetResponse
 import com.sbr.mdt.api.payees.PayeesGetResponse
 import com.sbr.mdt.api.transactions.TransactionsGetResponse
+import com.sbr.mdt.login.data.api.LoginRequest
+import com.sbr.mdt.login.data.api.LoginResponse
+import com.sbr.mdt.util.Constants
 import retrofit2.Call
 import retrofit2.Response
-import retrofit2.http.GET
-import retrofit2.http.Header
+import retrofit2.http.*
 
 interface MDTAPI {
     @GET("transactions")
-    suspend fun getTransactions(@Header("Authorization") token: String): Response<TransactionsGetResponse>
+    suspend fun getTransactions(@Header(Constants.AUTH_KEY) token : String?) : Response<TransactionsGetResponse>
 
     @GET("balance")
     suspend fun getBalance(
-        @Header("Authorization") token: String
-    ): Response<BalanceGetResponse>
+        @Header(Constants.AUTH_KEY) token : String?
+    ) : Response<BalanceGetResponse>
 
     @GET("payees")
-    suspend fun getPayees(@Header("Authorization") token: String): Response<PayeesGetResponse>
+    suspend fun getPayees(@Header(Constants.AUTH_KEY) token : String?) : Response<PayeesGetResponse>
 
+    @POST("login")
+    suspend fun login(@Body request : LoginRequest) : Response<LoginResponse>
 
-    @GET("balance")
-    fun getenqubalance(@Header("Authorization") token: String): Call<BalanceGetResponse>
 
 }
