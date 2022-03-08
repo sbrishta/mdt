df908094767abf0a3c3a49cc47b0fa993f585506
sbrishta <sbrishta.13@gmail.com>
2022-03-08 16:35:01 +0800

    unrelated class and models cleaned up.

--- app/src/main/java/com/sbr/mdt/login/ui/LoginViewModelFactory.kt
+++ app/src/main/java/com/sbr/mdt/login/ui/LoginViewModelFactory.kt
@@ -2,7 +2,6 @@ package com.sbr.mdt.login.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.sbr.mdt.login.data.LoginDataSource
 import com.sbr.mdt.login.data.LoginRepository
 
 /**
@@ -15,9 +14,7 @@ class LoginViewModelFactory : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass : Class<T>) : T {
         if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
             return LoginViewModel(
-                loginRepository = LoginRepository(
-                    dataSource = LoginDataSource()
-                )
+                loginRepository = LoginRepository()
             ) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
