cf4b458b4ce84bf7591e518e51339cb8e3f3cae8
sbrishta <sbrishta.13@gmail.com>
2022-03-08 20:31:25 +0800

    set up account holder, balance data to textview. formatted balance data as currency format

--- app/src/main/java/com/sbr/mdt/dashboard/ui/MainActivity.kt
+++ app/src/main/java/com/sbr/mdt/dashboard/ui/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.sbr.mdt.dashboard.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.sbr.mdt.R
+import com.sbr.mdt.dashboard.data.balance.BalanceGetResponse
 import com.sbr.mdt.dashboard.repository.TransactionBalanceRepository
-import com.sbr.mdt.databinding.ActivityLoginBinding
 import com.sbr.mdt.databinding.ActivityMainBinding
+import com.sbr.mdt.login.data.api.LoginResponse
+import com.sbr.mdt.util.Constants
 import com.sbr.mdt.util.Resource
 import com.sbr.mdt.util.SessionManager
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var viewModel : DashBoardViewModel
@@ -34,9 +37,17 @@ class MainActivity : AppCompatActivity() {
         val viewModelProviderFactory = DashBoardViewModelProviderFactory(repository)
         viewModel =
             ViewModelProvider(this, viewModelProviderFactory).get(DashBoardViewModel::class.java)
+
+       //-------set up user data from login response-----------
+        val userData = SessionManager.get<LoginResponse>(Constants.USER_DATA)
+        if (userData != null) {
+            accountNo.text = userData.accountNo
+            accountHolder.text = userData.username
+        }
+
         makeTransfer.setOnClickListener {
-            viewModel.getBalanceData()
-            viewModel.getTransactions()
+
+
         }
         logout.setOnClickListener{
             viewModel.logout()
@@ -44,7 +55,7 @@ class MainActivity : AppCompatActivity() {
         viewModel.userBalance.observe(this, Observer { response ->
             when (response) {
                 is Resource.Success -> {
-
+                    setBalanceData(response,balance)
                 }
                 is Resource.Error -> {
                     Log.d("BALANCe", response.message.toString())
@@ -67,5 +78,10 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         })
+    }
+    private fun setBalanceData(response:Resource<BalanceGetResponse>,balance:TextView){
+        val balanceData = response.data?.balance?.toDouble()
+        val formatted = java.lang.String.format(Locale.getDefault(),getString(R.string.currency_formatter), balanceData)
+        balance.text = formatted
     }
 }
